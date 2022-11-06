3248c81a59641def406e98fea18f5f203dc11f16
unknown <nvananh1805@gmail.com>
2022-11-06 19:14:27 +0700

    design homepage

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import VideoHomePage from '../../asset/video/video-1920.mp4'
+import '../../asset/css/components/home/home.scss'
 
 const HomePage = (props) => {
     return (
         <div className='homepage-container'>
-            <video autoPlay muted loop>
-                <source src={VideoHomePage} type='video/mp4'/>
-            </video>
+            <div>
+                <video autoPlay muted loop>
+                    <source src={VideoHomePage} type='video/mp4' />
+                </video>
+            </div>
+            <div className='container homepage-soloGan d-flex flex-column'>
+                <h4>
+                    Enjoy life with your book, turn your life into a library!
+                </h4>
+                <p>
+                    One can gain admittance to different classifications of books and different assets in the library.
+                    It avoids the need to purchase costly books and assets. Let the book connect to your inner self.
+                </p>
+                <button className='btn btn-dark'>Read Now</button>
+            </div>
         </div>
     )
 }
