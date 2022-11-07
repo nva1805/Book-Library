dfce36699a839fc41d6974c2ad515e880faf3cd6
unknown <nvananh1805@gmail.com>
2022-11-08 04:17:56 +0700

    design admin page

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import SideBar from './SideBar'
 import { ProSidebarProvider } from 'react-pro-sidebar';
+import '../../asset/css/components/admin/admin.scss'
 
 export default function Admin() {
   return (
     <div className='admin-container'>
+
       <div className='admin-title'>
         <div>
-          <h4 className='text-center display-4 bg-danger'>Welcome to Administration</h4>
+          <h4 className='text-center display-4 bg-dark text-light'>Welcome to Administration</h4>
         </div>
       </div>
+
       <div className="admin-dashboard d-flex">
         <div className='admin-sidebar'>
           <ProSidebarProvider>
--- src/components/Admin/SideBar.js
+++ src/components/Admin/SideBar.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import { Sidebar, Menu, MenuItem, SubMenu, useProSidebar } from 'react-pro-sidebar'; //import pro-side-bar to use
-// import { FaRegChartBar } from "react-icons/fa"; //import icon from react-icons lib
-import { BsFillBarChartFill } from "react-icons/bs";
+import { FaUsers, FaBook, FaSwatchbook } from "react-icons/fa"; //import icon from react-icons lib
+import {FcManager} from "react-icons/fc"; 
+import '../../asset/css/components/admin/sidebar.scss'
 
 
 const SideBar = () => {
     const { collapseSidebar } = useProSidebar();
     return (
-        <div style={{ display: 'flex', height: '100%' }}>
+        <div className='position-relative' style={{ height: '100%' }}>
             <Sidebar>
                 <Menu>
-                    <SubMenu icon={<BsFillBarChartFill />} label="Charts">
-                        <MenuItem> Pie charts </MenuItem>
-                        <MenuItem> Line charts </MenuItem>
+                    <SubMenu icon={<FcManager size={'2em'} />} label="Manager">
+                        <MenuItem icon={<FaUsers size={'1.5em'} />}> Manage Users </MenuItem>
+                        <MenuItem icon={< FaBook size={'1.5em'} />}> Manage Books </MenuItem>
+                        <MenuItem icon={< FaSwatchbook size={'1.5em'} />}> Manage Book Types </MenuItem>
                     </SubMenu>
                     <MenuItem> Documentation </MenuItem>
                     <MenuItem> Calendar </MenuItem>
                 </Menu>
             </Sidebar>
+
             <main>
-                <button className='btn btn-primary' onClick={() => collapseSidebar()}>Collapse</button>
+                <button className='btn btn-primary position-absolute' onClick={() => collapseSidebar()}>Collapse</button>
             </main>
         </div>
     )
