c84b74b4015496475dacc53dbe6170dd3dccb4d5
Funny Dev <gamevapha@gmail.com>
2022-11-16 02:17:14 +0700

    change css file to scss file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './asset/css/index.css';
+import './asset/css/index.scss';
 import App from './App';
 import reportWebVitals from './support/reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
