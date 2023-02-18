689f3ffbf75a5e6675098855078ddf0954faf992
nva1805 <nvananh1805@gmail.com>
2023-02-19 00:34:15 +0700

    complete home page when click button

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import VideoHomePage from '../../asset/video/video-1920.mp4'
 import '../../asset/css/components/home/home.scss'
+import { useNavigate } from 'react-router-dom'
+
 
 const HomePage = (props) => {
+    const navigate = useNavigate()
     return (
         <div className='homepage-container'>
             <div>
@@ -18,7 +21,9 @@ const HomePage = (props) => {
                     One can gain admittance to different classifications of books and different assets in the library.
                     It avoids the need to purchase costly books and assets. Let the book connect to your inner self.
                 </p>
-                <button className='btn btn-dark'>Read Now</button>
+                <button className='btn btn-dark'
+                onClick={() => navigate('novels')}
+                >Read Now</button>
             </div>
         </div>
     )
