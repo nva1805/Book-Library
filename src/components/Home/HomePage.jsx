ce32d86525b181dda418f8d1b66bc8c78c995493
nva1805 <nvananh1805@gmail.com>
2023-02-22 14:28:58 +0700

    change background video and set text-color, text-boxshadow color follow time

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
-import VideoHomePage from '../../asset/video/video-1920.mp4'
+import React, { useEffect, useState } from 'react'
+// import VideoHomePage from '../../asset/video/video-1920.mp4'
+import VideoHomePage from '../../asset/video/video-Intro.mp4'
 import '../../asset/css/components/home/home.scss'
 import { useNavigate } from 'react-router-dom'
 
 
 const HomePage = (props) => {
     const navigate = useNavigate()
+    const [textColor, setTextColor] = useState('white');
+  const [boxShadowColor, setBoxShadowColor] = useState('black');
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTextColor(textColor === 'white' ? 'black' : 'white');
+      setBoxShadowColor(boxShadowColor === 'black' ? 'white' : 'black');
+    }, 22860);
+
+    return () => clearInterval(intervalId);
+  }, [textColor, boxShadowColor]);
+
+  useEffect(() => {
+    const firstColorTimeoutId = setTimeout(() => {
+      setTextColor('black');
+      setBoxShadowColor('white');
+    }, 3550);
+    const secondColorTimeoutId = setTimeout(() => {
+      setTextColor('white');
+      setBoxShadowColor('black');
+    }, 22860);
+
+    return () => {
+      clearTimeout(firstColorTimeoutId);
+      clearTimeout(secondColorTimeoutId);
+    }
+  }, []);
     return (
-        <div className='homepage-container'>
+        <div className='homepage-container' style={{ color: textColor, textShadow: `0px 0px 10px ${boxShadowColor}` }}>
             <div>
                 <video autoPlay muted loop>
                     <source src={VideoHomePage} type='video/mp4' />
@@ -22,7 +50,7 @@ const HomePage = (props) => {
                     It avoids the need to purchase costly books and assets. Let the book connect to your inner self.
                 </p>
                 <button className='btn btn-dark'
-                onClick={() => navigate('novels')}
+                    onClick={() => navigate('novels')}
                 >Read Now</button>
             </div>
         </div>
